08f8b0d0bb2a8726cd3fe87baaa6991368766f8b
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-02-21 06:53:47 +0000

    edits to places views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,21 @@
 class PostsController < ApplicationController
-end
+
+    def index
+      @posts = Post.all
+    end
+  
+    def new
+      @post = Post.new
+    end
+  
+    def create
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["date"] = params["post"]["date"]
+      @post.save
+      redirect_to "/posts"
+    end
+  
+  end
+  
