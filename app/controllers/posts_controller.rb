b4ac5b01b0b91bd81d72d40820d666368f468feb
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-02-21 17:44:01 +0000

    I think it's working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,11 @@
 class PostsController < ApplicationController
 
-  def show
-    @post = Post.find_by({ "id" => params["id"] })
-    @place = Place.find_by({ "id" => @post["place_id"] })
+  def index
+    @posts = Post.all
   end
 
   def new
     @post = Post.new
-    @place = Place.find_by({ "id" => params["place_id"] })
-    @post["place_id"] = @place["id"]
   end
 
   def create
@@ -16,9 +13,9 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["contact"]["place_id"]
-    @contact.save
-    redirect_to "/places/#{@post["place_id"]}"
+    @post["place_id"] = params["post"]["place_id"]
+    @post.save
+    redirect_to "/places/"
   end
 
 end
