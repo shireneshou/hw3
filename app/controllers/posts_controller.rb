9f29a35bbfdae908d887c003a7b4d2e9b4e49cfc
shireneshou <shirene.shoushtarizadeh@kellogg.northwestern.edu>
2023-02-21 09:42:15 +0000

    about to give up

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,21 +1,24 @@
 class PostsController < ApplicationController
 
-    def index
-      @posts = Post.all
-    end
-  
-    def new
-      @post = Post.new
-    end
-  
-    def create
-      @post = Post.new
-      @post["title"] = params["post"]["title"]
-      @post["description"] = params["post"]["description"]
-      
-      @post.save
-      redirect_to "/posts"
-    end
-  
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => @post["place_id"] })
   end
-  
+
+  def new
+    @post = Post.new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    @post["place_id"] = @place["id"]
+  end
+
+  def create
+    @post = Post.new
+    @post["title"] = params["post"]["title"]
+    @post["description"] = params["post"]["description"]
+    @post["posted_on"] = params["post"]["posted_on"]
+    @post["place_id"] = params["contact"]["place_id"]
+    @contact.save
+    redirect_to "/places/#{@post["place_id"]}"
+  end
+
+end
